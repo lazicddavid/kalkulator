a95dc83cc3176d642ef19a0259597ae72379895c
David Lazic <lazic.ddavid@gmail.com>
2026-03-25 21:22:00 +0100

    Added numbersValue

--- kalkulator/src/main.js
+++ kalkulator/src/main.js
@@ -1,1 +1,16 @@
-const DOM = {};
+const DOM = {
+  buttons: document.querySelectorAll("button"),
+  currentValue: document.querySelector(".current-value"),
+  previousValue: document.querySelector(".previous-value"),
+  delete: document.querySelector(".delete"),
+  clear: document.querySelector(".clear"),
+};
+
+const numbersValues = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."];
+
+DOM.buttons.forEach(function (button) {
+  button.addEventListener("click", function () {
+    const value = button.textContent;
+    DOM.currentValue.textContent += value;
+  });
+});

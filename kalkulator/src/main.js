e8bf307f145bf2dd103edd74372ee1610423ffab
David Lazic <lazic.ddavid@gmail.com>
2026-03-29 18:18:16 +0200

    added renderNumber

--- kalkulator/src/main.js
+++ kalkulator/src/main.js
@@ -32,60 +32,13 @@ function renderNumber(value) {
 }
 
 
-DOM.buttons.forEach(
-   (button) {
-  button.addEventListener("click", function () {
-    const value = button.textContent;
 
-    if (numbersValues.includes(value)) {
-      DOM.currentValue.textContent += value;
-    }
-
-    if (operators.includes(value)) {
-      if (DOM.currentValue.textContent === "") return;
-
-      if (DOM.previousValue.textContent === "") {
-        operator = value;
-        DOM.previousValue.textContent =
-          DOM.currentValue.textContent + " " + value;
-        DOM.currentValue.textContent = "";
-      } else {
-        const previousText = DOM.previousValue.textContent;
-        const num1 = Number(previousText.split(" ")[0]);
-        const num2 = Number(DOM.currentValue.textContent);
-
-        let result = 0;
-
-        if (operator === "+") result = num1 + num2;
-        if (operator === "-") result = num1 - num2;
-        if (operator === "/") result = num1 / num2;
-        if (operator === "*") result = num1 * num2;
-
-        operator = value;
-        DOM.previousValue.textContent = result + " " + value;
-        DOM.currentValue.textContent = "";
-      }
-    }
-
-    if (value === "=") {
-      const previousText = DOM.previousValue.textContent;
-      const num1 = Number(previousText.split(" ")[0]);
-      const num2 = Number(DOM.currentValue.textContent);
-
-      let result = 0;
-
+/*
       if (operator === "-") result = num1 - num2;
       if (operator === "+") result = num1 + num2;
       if (operator === "*") result = num1 * num2;
       if (operator === "/") result = num1 / num2;
-
-      DOM.currentValue.textContent = result;
-      DOM.previousValue.textContent = "";
-      operator = "";
-    }
-  });
-});
-
+*/
 DOM.clear.addEventListener("click", function () {
   DOM.currentValue.textContent = "";
   DOM.previousValue.textContent = "";

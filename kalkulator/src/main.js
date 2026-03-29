85682c85938cc41cc77a5a8ee39c531371d665b8
David Lazic <lazic.ddavid@gmail.com>
2026-03-30 00:05:03 +0200

    Added render

--- kalkulator/src/main.js
+++ kalkulator/src/main.js
@@ -1,3 +1,8 @@
+//listeneri na brojeve
+//listeneri na operacije
+//state koji treba osim operacije
+//git
+
 const DOM = {
   numberButtons: document.querySelectorAll(".number"),
   operatorButtons: document.querySelectorAll(".operator"),
@@ -7,99 +12,27 @@ const DOM = {
   clear: document.querySelector(".clear"),
 };
 
-const operators = ["+", "-", "*", "/"];
-const numbersValues = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."];
-
-let operator = "";
-
 const state = {
   currentValue: "",
   previousValue: "",
+  operator: "",
+  number1: "",
+  number2: "",
 };
 
-function render() {
-  DOM.currentValue.textContent = state.currentValue;
-  DOM.previousValue.textContent = state.previousValue;
+//ova f-ja postavlja novu vrednost operatora u state
+function setOperator(value) {
+  state.operator = value;
 }
 
-function renderNumber(value) {
-  state.currentValue += value;
-  render();
+//cita trenutnu vrednost operatora iz state
+function getOperator() {
+  return state.operator;
 }
 
-function calculate(num1, num2) {
-  let result = 0;
-  if (operator === "-") result = num1 - num2;
-  if (operator === "+") result = num1 + num2;
-  if (operator === "*") result = num1 * num2;
-  if (operator === "/") result = num1 / num2;
-  return result;
+function setOperator(value) {
+  state.operator = value;
 }
 
-function renderOperator(value) {
-  if (state.currentValue === "") return;
-
-  if (state.previousValue === "") {
-    operator = value;
-    state.previousValue = state.currentValue + " " + value;
-    state.currentValue = "";
-  } else {
-    const previousText = state.previousValue;
-    const num1 = Number(previousText.split(" ")[0]);
-    const num2 = Number(state.currentValue);
-
-    const result = calculate(num1, num2);
-
-    operator = value;
-    state.previousValue = result + " " + value;
-    state.currentValue = "";
-  }
-  render();
-}
-
-function renderEqual() {
-  const previousText = state.previousValue;
-  const num1 = Number(previousText.split(" ")[0]);
-  const num2 = Number(state.currentValue);
-
-  const result = calculate(num1, num2);
-  state.currentValue = result;
-  state.previousValue = "";
-  operator = "";
-
-  render();
-}
-
-function handleClear() {
-  state.currentValue = "";
-  state.previousValue = "";
-  operator = "";
-  render();
-}
-
-DOM.clear.addEventListener("click", function () {
-  handleClear();
-});
-
-DOM.buttons.forEach(function (button) {
-  button.addEventListener("click", function () {
-    const value = button.textContent;
-
-    if (numbersValues.includes(value)) {
-      renderNumber(value);
-    }
-
-    if (operators.includes(value)) {
-      renderOperator(value);
-    }
-
-    if (value === "=") {
-      renderEqual();
-    }
-  });
-});
-
-//listeneri na brojeve
-//listeneri na operacije
-//state koji treba osim operacije
-//kako da ubacim projekat na git ?
+//prva funkcija, za kliktanje brojeva:
+ function render()

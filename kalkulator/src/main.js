1e8e9a1746e9d6748127700796717f3e70f26811
David Lazic <lazic.ddavid@gmail.com>
2026-04-10 22:36:41 +0200

    added state

--- kalkulator/src/main.js
+++ kalkulator/src/main.js
@@ -128,7 +128,8 @@ function handleSquare() {
   state.setCurrentValue(value * value);
   render();
 }
-render;
+state;
+
 function renderEqual() {
   const operator = state.getOperator();
   const current = state.getCurrentValue();
